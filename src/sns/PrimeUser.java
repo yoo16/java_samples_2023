8389ff5b82225f754e6bf218f8da01bac6e2c33b
yoo16 <yohei.yoshikawa@gmail.com>
2023-05-22 00:12:41 +0900

    update

---
+++ src/sns/PrimeUser.java
@@ -0,0 +1,7 @@
+package sns;
+
+import sns.entity.User;
+
+public class PrimeUser extends User {
+    
+}
--- src/sns/entity/App.java
+++ src/sns/entity/App.java
@@ -3,10 +3,12 @@ package sns.entity;
 public class App {
     public static void main(String[] args) {
         User user = new User();
-        user.name = "東京 太郎";
         user.email = "test@example.com";
-        user.password = "pass12345";
 
+
+        // user.name = "東京 太郎";
+        // user.email = "test@example.com";
+        // user.password = "pass12345";
 
         // user.setName("東京　太郎");
         // user.setEmail("test@example.com");
